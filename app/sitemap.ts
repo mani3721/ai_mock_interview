a8370f9ffb7d7fd5ac2b8174e146ab830790ed3f
Petchimuthu <petchimuthu.p@zohocorp.com>
2025-04-06 15:09:45 +0530

    SEO Next js

---
+++ app/sitemap.ts
@@ -0,0 +1,15 @@
+import { MetadataRoute } from "next";
+
+export default async function sitemap():Promise<MetadataRoute.Sitemap> {
+
+    return [
+        {
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+            lastModified: new Date()
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_BASE_URL}/interview`,
+            lastModified: new Date(),
+        },
+    ]
+}
