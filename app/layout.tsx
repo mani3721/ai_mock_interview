a5adf5a7b1dad061a678139020d4378d0dd18714
Petchimuthu <petchimuthu.p@zohocorp.com>
2025-04-06 16:19:16 +0530

    add analitics

--- app/layout.tsx
+++ app/layout.tsx
@@ -34,6 +34,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
+      <head>
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-FEDVZZXDJB"></script>
+          <script>
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-FEDVZZXDJB');
+
+            `}
+          </script>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         {children}
